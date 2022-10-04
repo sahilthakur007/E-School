e237c1ca07e74cb48123e3c5fd9479ecaa06742c
sahil thakur <st7378539838@gmail.com>
2022-10-04 23:24:58 +0530

    deployment

--- onlineAssignmentSubmission/onlineAssignmentSubmission/settings.py
+++ onlineAssignmentSubmission/onlineAssignmentSubmission/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,10 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS=[
-    BASE_DIR,"static"
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS=[
+#     BASE_DIR,"static"
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+django_heroku.settings(locals())
