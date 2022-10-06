15f6ec9e5bcc375e3f1556306d976588271eb9a6
divyakekade <kekadedivya@gmail.com>
2022-10-06 17:20:15 +0530

    new

--- onlineAssignmentSubmission/onlineAssignmentSubmission/settings.py
+++ onlineAssignmentSubmission/onlineAssignmentSubmission/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=[
-    BASE_DIR,"static"
-]
+# STATICFILES_DIRS=[
+#     BASE_DIR,"static"
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
